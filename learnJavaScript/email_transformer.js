4859b08073f144a71f63d40868f215826dd4a3ce
SirArslaan <arhamoja@gmail.com>
2014-08-07 16:48:01 +0200

    green

--- learnJavaScript/email_transformer.js
+++ learnJavaScript/email_transformer.js
@@ -1,7 +1,11 @@
 function transform(text) {
+  var initialText = text;
   if(text.length>12){
     for (var key in Rules()){
       text = text.replace(key,Rules()[key]);
+    }
+    if(!isValidEmail(text)){
+      return initialText;
     }
   }
   return text;
@@ -16,3 +20,12 @@ function Rules(){
   };
   return self;
 }
+
+var isValidEmail = function(text) {
+  for(var i=0; i<text.length-2;i++) {
+    if( text[i]+text[i+1]== "@." ){
+      return false;
+    }
+  }
+  return true;
+} 
