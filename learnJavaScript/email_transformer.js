82ac1e190fb89e29ce5f6a9097b49b85158ce6f2
SirArslaan <arhamoja@gmail.com>
2014-08-07 16:18:44 +0200

    green

--- learnJavaScript/email_transformer.js
+++ learnJavaScript/email_transformer.js
@@ -2,6 +2,8 @@ function transform(text) {
   if(text.length>12){
     text = text.replace("(AT)","@");
     text = text.replace('(DOT)',".");
+    text = text.replace('-AT-','@');
+    text = text.replace('-DOT-',".");
   }
   return text;
 }
